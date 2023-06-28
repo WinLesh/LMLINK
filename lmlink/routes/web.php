ea291c1e3b9ba1400208f7f89dcdb2e1578ed421
Michael Erameh <eramehmichael2000@gmail.com>
2023-06-28 16:10:08 +0100

    some few changes

--- lmlink/routes/web.php
+++ lmlink/routes/web.php
@@ -17,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function(){
-    return view('home');
-});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
